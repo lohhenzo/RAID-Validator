53a881b1d1d53950ed46cce794ed89d4571c5571
Lorenzo Hernandez <lohhenzo@gmail.com>
2015-04-16 17:44:55 -0600

    Integrate tornado webserver as interface to the RAID module, add test unit to test the WS

--- server.py
+++ server.py
@@ -5,12 +5,16 @@ import raid
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write(raid.main())
+        self.write('test')
+
+    def post(self):
+        po = self.get_argument('po')
+        self.write(str(raid.main(po)))
 
 application = tornado.web.Application([
     (r"/", MainHandler),
 ])
 
 if __name__ == "__main__":
-    application.listen(8889)
+    application.listen(8890)
     tornado.ioloop.IOLoop.instance().start()
--- test-unit.py
+++ test-unit.py
@@ -1,7 +1,7 @@
-import os
+import os,urllib, urllib2
 import raid
 
-path = '/home/lorenzo/projects/RAIDValidation/GCF_52/'
+path = '/home/lorenzo/projects/assets/GCF_52/'
 
 def testMultiple():
     gcfList = {}
@@ -24,7 +24,18 @@ def testOne(filename):
 
     print raid.main(gcf)
 
-testMultiple()
+def testWS(filename):
+    gcf = None
+    with open(path + filename + '.xml', 'r') as file:
+        gcf = file.read()
+
+    parameters = { 'po': gcf }
+    arguments = urllib.urlencode(parameters)
+    response = urllib2.urlopen('http://localhost:8890/', arguments)
+    print response.read()
+
+#testMultiple()
 #testOne('20674984470_1')
 #testOne('20675014450_1')
 #testOne('20673569920_1')
+testWS('20673569920_1')
